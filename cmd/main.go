703b710348e761065930053160ca0de7d14bcf99
Sebastian Martínez <sebasmartinez05@outlook.com>
2025-06-13 03:36:36 +0000

    add comments to explain what i'm doing

--- cmd/main.go
+++ cmd/main.go
@@ -8,10 +8,15 @@ import (
 )
 
 func main() {
+	// Create a new router
 	r := chi.NewRouter()
+	// Use middleware for logging (this is made from chi)
 	r.Use(middleware.Logger)
+	// Create a simple GET route with a string response "Hello World!"
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		// byte is used to convert string to bytes
 		w.Write([]byte("Hello World!"))
 	})
-	http.ListenAndServe(":3000", r)
+	// Start the HTTP server on port 8080
+	http.ListenAndServe(":8080", r)
 }
